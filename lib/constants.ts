f586db98d5307eeb1bd7140862217e658ce7622f
Jack <jack@shapeng1998.com>
2021-12-24 21:31:04 +0800

    fix: fix revalidation time

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const INITIAL_PAGE_NUMBER = 1; // current page number
 
-export const REVALIDATE_TIME = 10; // 10 seconds
+export const REVALIDATE_TIME = 1; // 1 seconds
 
-export const CACHE_EXPIRATION_TIME = 10000; // 10 seconds
+export const CACHE_EXPIRATION_TIME = 2000; // 2 seconds
